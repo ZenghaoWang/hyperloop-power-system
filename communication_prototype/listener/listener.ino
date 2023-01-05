77e0693a8b6de660924bc03861b9dabb66627347
AlexandreSinger <alex.singer@mail.utoronto.ca>
2023-01-05 15:36:57 -0500

    Increased the bit rate and cleaned up the code
    Tested the bit rate at 1 Mbps instead of 250 kbps with 3 nodes
    instead of 2. The messages were being sent over the channel
    without aparent corruption or loss. Will need to check with an
    oscilloscope to determine if the channel is safe.

--- communication_prototype/listener/listener.ino
+++ communication_prototype/listener/listener.ino
@@ -16,36 +16,32 @@ void setup() {
   // Set up the serial interface
   Serial.begin(115200);
 
-  // Set up the can bus interface to run at 250 Kbps
-  Can0.begin(CAN_BPS_250K);
+  // Set up the can bus interface to run at 1000 Kbps
+  Can0.begin(CAN_BPS_1000K);
 
-  //By default there are 7 mailboxes for each device that are RX boxes
-  //This sets each mailbox to have an open filter that will accept extended
-  //or standard frames
-  int filter;
-  //extended
-  for (filter = 0; filter < 3; filter++) {
-    Can0.setRXFilter(filter, 0, 0, true);
-    Can1.setRXFilter(filter, 0, 0, true);
-  }  
-  //standard
-  for (int filter = 3; filter < 7; filter++) {
-    Can0.setRXFilter(filter, 0, 0, false);
-    Can1.setRXFilter(filter, 0, 0, false);
-  }  
+  // There are 7 mailboxes for each device that are RX boxes.
+  // Set each mailbox to have an open filter that will accept standard
+  // (non-extended) frames only.
+  // Standard frames allow for an 11-bit identifier allowing for a total of 
+  // 2048 different.
+  // Extended frames allow for a 29-bit identifier allowing for a total of
+  // 536+ million messages.
+  for (int filter = 0; filter < 7; filter++) {
+    Can0.setRXFilter(filter, 0, 0, false);    
+  }
 }
 
 void printFrame(CAN_FRAME &frame) {
-   Serial.print("ID: 0x");
-   Serial.print(frame.id, HEX);
-   Serial.print(" Len: ");
-   Serial.print(frame.length);
-   Serial.print(" Data: 0x");
-   for (int count = 0; count < frame.length; count++) {
-       Serial.print(frame.data.bytes[count], HEX);
-       Serial.print(" ");
-   }
-   Serial.print("\r\n");
+  Serial.print("ID: 0x");
+  Serial.print(frame.id, HEX);
+  Serial.print(" Len: ");
+  Serial.print(frame.length);
+  Serial.print(" Data: 0x");
+  for (int count = 0; count < frame.length; count++) {
+    Serial.print(frame.data.bytes[count], HEX);
+    Serial.print(" ");
+  }
+  Serial.print("\r\n");
 }
 
 void loop(){
@@ -53,10 +49,6 @@ void loop(){
 
   if (Can0.available() > 0) {
     Can0.read(incoming); 
-    printFrame(incoming);
-  }
-  if (Can1.available() > 0) {
-    Can1.read(incoming); 
     printFrame(incoming);
   }
 }
--- communication_prototype/sensor/sensor.ino
+++ communication_prototype/sensor/sensor.ino
@@ -3,16 +3,21 @@
 
   Sends fake sensor data onto the CAN bus to simulate a sensor on the pod.
 
+  The LED on the board will toggle everytime a message is sent onto the bus.
+
   Alexandre Singer
 */
 
 #include "variant.h"
 #include <due_can.h>
 
+// The delay between sending sensor data to the CAN bus in milliseconds.
+#define DELAY_IN_MS 1000
+
 unsigned light;
 
 void setup() {
-  Can0.begin(CAN_BPS_250K);
+  Can0.begin(CAN_BPS_1000K);
 
   // Debug: setup the led on the board to blink
   pinMode(LED_BUILTIN, OUTPUT);
@@ -21,22 +26,29 @@ void setup() {
 
 void sendData()
 {
-	CAN_FRAME outgoing;
-	outgoing.id = 0x400;
-	outgoing.extended = false;
-	outgoing.priority = 4; // 0-15 lower is higher priority
-	
-  outgoing.length = 8;
-	outgoing.data.s0 = 0xFEED;
+  CAN_FRAME outgoing;
+  outgoing.id = 0x400;        // The ID of the frame
+  outgoing.extended = false;  // Whether the ID is extended or not
+  outgoing.priority = 2;      // 0-15 lower is higher priority
+  outgoing.length = 8;        // The length of the data in the frame
+
+  // The data field is a maximum of 8 bytes in length
+  // Can be accessed in 3 different ways:
+  //    1) using .s0 / .s1 / .s2 / .s3 to access each of the 4 (2 byte) words at a time
+  outgoing.data.s0 = 0xFEED;        // The first word of data
+  //    2) using 'byte' or 'bytes' arrays to access each of the 8 bytes at a time
   outgoing.data.byte[2] = 0xDD;
-	outgoing.data.byte[3] = 0x55;
-	outgoing.data.high = 0xDEADBEEF;
-	Can0.sendFrame(outgoing);
+  outgoing.data.byte[3] = 0x55;
+  //    3) using 'low' or 'high' to access the lower or upper 4 bytes respectively
+  outgoing.data.high = 0xDEADBEEF;
+
+  // Send the frame over the bus
+  Can0.sendFrame(outgoing);
 }
 
 void loop() {
   static unsigned long lastTime = 0;
-  if ((millis() - lastTime) > 1000) {
+  if ((millis() - lastTime) > DELAY_IN_MS) {
     lastTime = millis();
     sendData();
     // Toggle the light
