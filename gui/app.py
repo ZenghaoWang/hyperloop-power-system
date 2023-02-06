e18ef568f59151d4b62aa4dde24b776bbc8f78d3
Zenghao Wang <zenghao.wang@mail.utoronto.ca>
2023-02-05 19:58:50 -0500

    Add generated interface code, including line graphs

--- gui/app.py
+++ gui/app.py
@@ -1,26 +1,35 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLabel, QWidget
+from interface import Ui_MainWindow
 from PyQt5.QtCore import Qt
-
-class Label(QLabel):
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-
-		self.setAlignment(Qt.AlignCenter)
 
 class MainWindow(QMainWindow):
 	def __init__(self):
-		super().__init__()
+		super(MainWindow, self).__init__()
+		self.ui = Ui_MainWindow()
+		self.ui.setupUi(self)
 
-		self.setWindowTitle("Hyperloop Power System Testing Interface")
-		main_layout = QVBoxLayout()
-		main_layout.addWidget(Label("Hi"))
 
-		main_widget = QWidget()
-		main_widget.setLayout(main_layout)
 
-		self.setCentralWidget(main_widget)
+# class Label(QLabel):
+# 	def __init__(self, *args, **kwargs):
+# 		super().__init__(*args, **kwargs)
+
+# 		self.setAlignment(Qt.AlignCenter)
+
+# class MainWindow(QMainWindow):
+# 	def __init__(self):
+# 		super().__init__()
+
+# 		self.setWindowTitle("Hyperloop Power System Testing Interface")
+# 		main_layout = QVBoxLayout()
+# 		main_layout.addWidget(Label("Hi"))
+
+# 		main_widget = QWidget()
+# 		main_widget.setLayout(main_layout)
+
+# 		self.setCentralWidget(main_widget)
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
