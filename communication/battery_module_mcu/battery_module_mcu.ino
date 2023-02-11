77c9e6af389e52f71a0b3f8073ab3b0ebae0a16a
AlexandreSinger <alex.singer@mail.utoronto.ca>
2023-02-11 11:03:59 -0500

    [Communication] Removed SoC from Battery Module Code
    Through discussions with the team and UTHT, found that the SoC what
    difficult to implement and not needed for this design. Removed from
    the code.

--- communication/battery_module_mcu/battery_module_mcu.ino
+++ communication/battery_module_mcu/battery_module_mcu.ino
@@ -1,8 +1,7 @@
 /*
   battery_module_mcu
 
-  Monitors the temperature and voltage of the battery module within the UTHT
-  Hyperloop pod.
+  Monitors the temperature of the battery module within the UTHT Hyperloop pod.
 
   Designed to run on an Arduino Micro, communicates directly with an Arduino
   Due over I2C (the Micro is the slave and the Due is the master).
@@ -17,8 +16,6 @@
 //   Will be different for EACH Arduino Micro.
 //   Can be any value from 0 - 127 (7-bit address).
 #define I2C_ADDR 0
-// The analog input port used to measure the voltage.
-#define VOLTAGE_ANALOG_INPUT_PORT A7
 // The analog input ports used to measure temperature.
 #define TEMP_ANALOG_INPUT_PORTS {A0, A1, A2, A3, A4, A5, A6}
 // The number of analog input ports used to measure temperature.
@@ -54,22 +51,6 @@ static inline float convert_sensor_value_to_temp(int analog_sensor_value) {
   return 62.9036 * voltage_squared - 312.17635 * voltage + 387.51705;
 }
 
-// Convert an analog sensor value into a State of Charge value using a linear
-// interpolation to get the state of charge from the voltage of the battery
-// with the max state of charge (1) when the batteries are at 29.4V and min (0)
-// when the batteries are at 17.5V.
-// TODO: Update this based on experimental results to use a more accurate
-//       model. The real SoC characteristics will depend on the discharge
-//       characteristics of the batteries based on their C values.
-static inline float convert_sensor_value_to_SoC(int analog_sensor_value) {
-  float port_voltage = convert_sensor_value_to_voltage(analog_sensor_value);
-  // Assuming that the voltage analog input port is reading the voltage accross
-  // a voltage divider with 29.4V corresponding to the maximum value of 2.56V,
-  // the battery voltage must be scaled up accordingly.
-  float battery_voltage = port_voltage * (29.4 / 2.56);  
-  float state_of_charge = 0.084 * battery_voltage - 1.4706;
-}
-
 // Callback function for when data is requested from this device by the master.
 //  Calculates the temperature and voltage of the battery module and sends it
 //  to the master.
@@ -87,15 +68,8 @@ void I2C_Request_Handler() {
   }
   float max_battery_temp = convert_sensor_value_to_temp(min_temp_sensor_val);
 
-  // Get the state of charge of the battery module using the battery voltage.
-  int voltage_sensor_value = analogRead(VOLTAGE_ANALOG_INPUT_PORT);
-  float state_of_charge = convert_sensor_value_to_SoC(voltage_sensor_value);
-
   // Send the data over the I2C interface.
-  //  The data is sent as an array of 8 chars, but is actually an array of 2
-  //  floats. Need to be careful on the receiver side.
-  float data[] = {max_battery_temp, state_of_charge};
-  Wire.write((char *)data, sizeof(data));
+  Wire.write((char *)(&max_battery_temp), sizeof(float));
 }
 
 void loop() {
